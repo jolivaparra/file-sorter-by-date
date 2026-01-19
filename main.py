e57cdc08ec0dcb0e720da421403461770d36dc9a
Joshua Oliva <joliva2024@udec.cl>
2026-01-18 23:57:10 -0300

    feat: add support for reading paths from config.txt

--- main.py
+++ main.py
@@ -2,9 +2,20 @@ from pathlib import Path
 import re
 import shutil
 
-root = Path('test')
-source = root / 'source'
-destination = root / 'destination'
+config_file = Path('config.txt')
+
+if config_file.exists():
+    with open(config_file, 'r') as configs:
+        content = configs.read()
+        src = re.search(r'Source: "(.*)"', content)
+        dst = re.search(r'Destination: "(.*)"', content)
+
+        if src and dst:
+            source = Path(src.group(1))
+            destination = Path(dst.group(1))
+else:
+    print('Archivo de configuraciones no encontrado.')
+    input()
 
 regex = re.compile(r'(20\d{2})(\d{2})(\d{2})_\d+')
 
@@ -12,9 +23,7 @@ files = source.glob('*.*')
 days = []
 
 for file in files:
-
     info = regex.search(file.stem)
-    
     if info:
         days.append(info.groups())
 
